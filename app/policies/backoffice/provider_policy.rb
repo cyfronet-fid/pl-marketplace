4828f78d238c949d88ae70bd49cead4a411d64b3
Maria <maria77julia@gmail.com>
2025-02-19 13:08:04 +0100

    feat:[#129] Provider creation wizard form
    Add new simplier wizard form
    for the provider using turbo_streams
    From now position of flash messages
    is fixed on the top
    
    Closes #129

--- app/policies/backoffice/provider_policy.rb
+++ app/policies/backoffice/provider_policy.rb
@@ -2,15 +2,28 @@
 
 class Backoffice::ProviderPolicy < Backoffice::ApplicationPolicy
   def new?
-    super || catalogue_access?
+    user.present?
   end
 
   def create?
-    super || catalogue_access?
+    user.present?
+  end
+
+  def edit?
+    edit_permissions?
+  end
+
+  def update?
+    edit_permissions?
+  end
+
+  def exit?
+    user.present?
   end
 
   def permitted_attributes
     [
+      :current_step,
       :name,
       :abbreviation,
       :website,
@@ -49,8 +62,20 @@ class Backoffice::ProviderPolicy < Backoffice::ApplicationPolicy
       [societal_grand_challenge_ids: []],
       [national_roadmaps: []],
       [sources_attributes: %i[id source_type eid _destroy]],
-      [main_contact_attributes: %i[id first_name last_name email phone organisation position]],
-      [public_contacts_attributes: %i[id first_name last_name email phone organisation position _destroy]],
+      [main_contact_attributes: %i[id first_name last_name email phone country_phone_code organisation position]],
+      [
+        public_contacts_attributes: %i[
+          id
+          first_name
+          last_name
+          email
+          phone
+          country_phone_code
+          organisation
+          position
+          _destroy
+        ]
+      ],
       [data_administrators_attributes: %i[id first_name last_name email _destroy]],
       [link_multimedia_urls_attributes: %i[id name url _destroy]],
       [alternative_identifiers_attributes: %i[id identifier_type value _destroy]]
@@ -61,6 +86,10 @@ class Backoffice::ProviderPolicy < Backoffice::ApplicationPolicy
 
   def catalogue_access?
     user&.catalogue_owner?
+  end
+
+  def edit_permissions?
+    !record.deleted? && (coordinator? || record&.owned_by?(user))
   end
 
   def access?

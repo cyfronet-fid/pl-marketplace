2233c31c9e44f0e232f1e932f7e9ef4a5c49827f
Paweł Gorczyca <goreck888@gmail.com>
2025-05-05 12:12:36 +0200

    feat:[#3414] Add node field and fix integration
    Fix integration with Provider's Component
    add Node reading and writing in the db as a Vocabulary
    Fit routes to the new pid format
    
    Closes #3414

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -127,4 +127,11 @@ module ApplicationHelper
       "bottom"
     end
   end
+
+  def unescaped_link_to(name = nil, raw_url = nil, html_options = nil)
+    parser = URI::Parser.new
+    url = parser.unescape(url_target(name, raw_url))
+    puts "ELO #{raw_url.class}"
+    link_to(name, url, html_options)
+  end
 end

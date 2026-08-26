6bb4dbb292afea1bcb555678f3f4620c8fad0d8d
szymon figura <szymon.figura@cyfronet.pl>
2026-08-26 12:52:32 +0200

    fix: adapt commons header to use defaults and fix styles

--- app/models/concerns/logo_attachable.rb
+++ app/models/concerns/logo_attachable.rb
@@ -19,7 +19,7 @@ module LogoAttachable
     File.open(image_path, "rb") do |file|
       io = File.extname(image_path) == ".png" ? StringIO.new(file.read) : convert_to_png(file)
 
-      logo.attach(io: io, filename: "#{SecureRandom.uuid}.png" , content_type: "image/png")
+      logo.attach(io: io, filename: "#{SecureRandom.uuid}.png", content_type: "image/png")
     end
   end
 

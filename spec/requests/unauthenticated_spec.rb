d65484d22f33b834a7b461884c3e9c4c2cadf26a
mich.kolomanski <ymkolom@cyf-kr.edu.pl>
2026-09-16 14:56:03 +0200

    fix: unauthenticated user tests

--- spec/requests/unauthenticated_spec.rb
+++ spec/requests/unauthenticated_spec.rb
@@ -3,8 +3,6 @@
 require "rails_helper"
 
 RSpec.describe "Unauthenticated user", backend: true do
-  include OmniauthHelper
-
   context "when accessing a protected profile page" do
     before { get profile_path }
 
@@ -30,35 +28,6 @@ RSpec.describe "Unauthenticated user", backend: true do
 
     it "preserves the requested Backoffice page" do
       expect(request.session["user_return_to"]).to eq(backoffice_services_path)
-    end
-
-    context "when Check-In authentication is successful" do
-      let(:user) { create(:user, roles: [:coordinator]) }
-
-      before do
-        stub_checkin(user)
-        get user_checkin_omniauth_authorize_path
-      end
-
-      it "redirects to the OmniAuth callback" do
-        expect(response).to redirect_to(user_checkin_omniauth_callback_path)
-      end
-
-      context "when following the Check-In callback" do
-        before { follow_redirect! }
-
-        it "redirects to the requested Backoffice page" do
-          expect(response).to redirect_to(backoffice_services_path)
-        end
-
-        context "when following the Backoffice redirect" do
-          before { follow_redirect! }
-
-          it "allows access to Backoffice services" do
-            expect(response).to have_http_status(:ok)
-          end
-        end
-      end
     end
   end
 end
